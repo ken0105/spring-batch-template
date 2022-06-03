26de2eb2df9e1863d7041f4085c92cb73abf8f2c
Ken Iwata <iwata.kenaaa@gmail.com>
2022-06-04 00:44:30 +0900

    Initial Commit

---
+++ src/main/java/com/ken0105/springbatchtemplate/SpringBatchTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ken0105.springbatchtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchTemplateApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/db/migration/V1.0.0__create_employee.sql
@@ -0,0 +1,7 @@
+create table if not exists employee
+(
+    id     int primary key,
+    name   varchar(50),
+    age    int,
+    gender int
+);
